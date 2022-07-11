31dd5bce969c0a03672f62be0eff899182169348
Vitor Riboli <vitorriboli1@gmail.com>
2022-07-11 18:42:21 -0300

    Project Created

---
+++ backend/src/test/java/com/vitor/metasms/MetasmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.metasms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
