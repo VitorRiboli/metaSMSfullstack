51fe8cd7797bfc22db957d2e00d3ee8c08a0342e
Vitor Riboli <vitorriboli1@gmail.com>
2022-07-15 19:57:02 -0300

    Feat: Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,19 +4,23 @@ import React, { useEffect, useState } from 'react'
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 
 const SalesCard = () => {
-    
+
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const max = new Date();
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]); //useState tipado que vai ser uma lista [] vazia
+
     useEffect(() => { /*Fazendo requisição para o backend */
-        axios.get("https://meta-sms.herokuapp.com/sales") // vai retornar um objeto promisse
+        axios.get(`${BASE_URL}/sales`) // vai retornar um objeto promisse
             .then(response => {
-                console.log(response.data)
+                setSales(response.data.content);
             })
     }, []) /*Por padrao, por estar rodando em ambiente DEV ele imprimi duas veses*/
 
@@ -56,49 +60,24 @@ const SalesCard = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show960">#1</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show960">10</td>
-                            <td className="show960">10</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show960">#1</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show960">10</td>
-                            <td className="show960">10</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show960">#1</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show960">10</td>
-                            <td className="show960">10</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show960">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show960">{sale.visited}</td>
+                                    <td className="show960">{sale.deals}</td>
+                                    <td>R$ ${sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
-
-
                 </table>
             </div>
 
